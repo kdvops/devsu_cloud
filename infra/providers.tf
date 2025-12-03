47e600c072abff3ab21e3f3a4b67ccd92cdb7005
kdvops <kelvin.ag89@gmail.com>
2025-12-03 00:19:39 -0400

    Create providers.tf

---
+++ infra/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
